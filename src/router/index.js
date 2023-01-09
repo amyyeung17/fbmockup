46c66f923ad333f791e401b1c5fbff94338f9102
Amy Yeung <amyyeung17@gmail.com>
2023-01-09 16:32:16 -0500

    Added base in router

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 
 const router = createRouter({
+  base: '/project3',
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
