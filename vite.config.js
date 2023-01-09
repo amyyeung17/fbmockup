16471d3a3f662c259a19c13bce1db34d54d14ac1
Amy Yeung <amyyeung17@gmail.com>
2023-01-09 16:01:54 -0500

    added base path

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vueJsx from "@vitejs/plugin-vue-jsx";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/project3',
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
